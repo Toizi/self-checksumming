5ca647adb75345cbfafce3179e99f3de5f47b9dd
Marius Guggenmos <MariusGuggenmos@gmail.com>
2019-08-08 17:02:29 +0200

    cleaned up ghidra_patch + added support for mangled names

--- patcher/ghidra_patch.py
+++ patcher/ghidra_patch.py
@@ -32,13 +32,6 @@ def find_placeholder_sequential(mm,start_index,struct_flag, search_value):
     addr = mm.find(search_bytes,start_index)
     return addr
 
-# def find_placeholder(mm,struct_flag,search_value):
-#     search_bytes = struct.pack(struct_flag, search_value)
-#     addr = mm.find(search_bytes)
-#     if addr == -1:
-#         mm.seek(0)
-#     addr = mm.find(search_bytes)
-#     return addr
 def find_placeholder(struct_flag, func, search_value):
     # build a search string for findBytes
     # 0x1337 => r'\x13\x37'
@@ -52,10 +45,14 @@ def find_placeholder(struct_flag, func, search_value):
 
 def patch_address(addr, patch_value):
     instr = getInstructionContaining(toAddr(addr))
-    instr_addr = instr.getAddress()
-    clearListing(instr_addr)
+    if instr:
+        instr_addr = instr.getAddress()
+        clearListing(instr_addr)
+
     setBytes(toAddr(addr), patch_value)
-    disassemble(instr_addr)
+
+    if instr:
+        disassemble(instr_addr)
 
 def find_address_of_placeholders(patch, checker_function):
     for placeholder in ('hashaddr_placeholder', 'hash_placeholder', 'size_placeholder'):#patch:
@@ -91,22 +88,6 @@ def find_all_placeholders(guard_patches):
             if placeholder_value not in placeholder_addresses:
                 placeholder_addresses[placeholder_value] = address
 
-            # while address is not None:
-            #     placeholder_addresses[placeholder_value].append(address)
-            #     start_index = address+1 
-            #     address = find_placeholder_sequential(mm, start_index, struct_flag, placeholder_value)
-                
-    # found_addresses = 0
-    # pmap = {}
-    # for paddress in placeholder_addresses:
-    #     count_placeholders = len(placeholder_addresses[paddress])
-    #     for address in placeholder_addresses[paddress]:
-    #         if address in pmap:
-    #             pholder = pmap[address] 
-    #             if pholder != paddress: 
-    #                 print("ERR. Same address mapped to two placehoders", pholder, paddress)
-    #                 return False
-    #     found_addresses = found_addresses + count_placeholders
     return placeholder_addresses
 
 
@@ -129,81 +110,72 @@ def patch_placeholder(struct_flag, addr_to_patch, target_value, patch, name_to_i
     return num_patches_applied
 
 
+function_cache = {}
+def find_function(func_name):
+    global function_cache
+
+    # see if it's cached first
+    func = function_cache.get(func_name)
+    if func:
+        return func
+
+    # use regular getFunction
+    func = getFunction(func_name)
+    if func:
+        function_cache[func_name] = func
+        return func
+    
+    
+    # in case of mangled names e.g. getFunction does not find it
+    # therefore we check if a symbol with that name exists
+    sym = getSymbol(func_name, None)
+    if sym is None:
+        function_cache[func_name] = None
+        return None
+
+    func = getFunctionAt(sym.getAddress())
+    function_cache[func_name] = func
+
+    return func
+
+def get_function_size(func):
+    body = func.getBody()
+    return body.getMaxAddress().getOffset() - body.getMinAddress().getOffset()
+
 def get_patches(input_file, guide_content):
-    # find addresses and sizes of all functions
-    # function_list = r2.cmdj("aflj")
-    # funcs = {}
-    # for function in function_list:
-    #     attr = {'size':function['size'], 'offset':function['offset']}
-    #     funcs[function['name']] = attr
-    funcs = {}
-    func = getFirstFunction()
-    while func is not None:
-        body = func.getBody()
-        size = body.getMaxAddress().getOffset() - body.getMinAddress().getOffset()
-        funcs[func.getName()] = {
-            'size': size,
-            'offset': func.getEntryPoint().getOffset(),
-            'function': func
-            }
-        func = getFunctionAfter(func)
         
     patches = []
     for c in guide_content:
         s = c.split(',')
-        checker_func = s[0].rstrip('_')
-        target_func = s[1].rstrip('_')
+        checker_func_name = s[0]
+        target_func_name = s[1]
         hashaddr_placeholder = int(s[2])
         size_placeholder = int(s[3])
         hash_placeholder = int(s[4])
 
-        if target_func in funcs and checker_func in funcs:
-            offset = funcs[target_func]['offset']
-            size = funcs[target_func]['size']
-            patch = {'hashaddr_placeholder': hashaddr_placeholder, 
+        
+        target_func = find_function(target_func_name)
+        checker_func = find_function(checker_func_name)
+
+        if target_func and checker_func:
+            patch = {
+                'hashaddr_placeholder': hashaddr_placeholder, 
                 'hashaddr_address': 0, # address of the hash
                 'size_placeholder': size_placeholder, 
                 'size_address': 0, # address of the length that should be hashed
                 'hash_placeholder': hash_placeholder,
                 'hash_address': 0, # address of the hash value
-                'checkee_addr' : offset, # address of the function that will be hashes
-                'checker_function' : funcs[checker_func]['function'].getName(), # name of the function that contains the check
-                'size_target': size, # how many bytes should be hashed
+                'checkee_addr' : target_func.getEntryPoint().getOffset(), # address of the function that will be hashes
+                'checker_function' : checker_func.getName(), # name of the function that contains the check
+                'size_target': get_function_size(target_func), # how many bytes should be hashed
                 'expected_hash': 0,
                 'dummy': False
             }
-            if not find_address_of_placeholders(patch, funcs[checker_func]['function']):
+            if not find_address_of_placeholders(patch, checker_func):
                 print('ERR: find_address_of_placeholders failed')
             patches.append(patch)
-
-        # else:
-        #     # required due to r2 bug that seeks to wrong function
-        #     # for some reason if af is not called before
-        #     r2.cmd('af ' + target_func)
-        #     r2.cmd('s ' + target_func)
-        #     funcinfo = r2.cmdj('afij')
-        #     if funcinfo:
-        #         print('TAKING ME')
-        #         print(funcinfo)
-        #         func_info = funcinfo[0]
-        #         error = False
-        #         if target_func.replace('sym.','') != func_info['name'].replace('sym.',''):
-        #             if func_info['name'].replace('sym.', '') == 'entry0':
-        #                 dump_debug_info('WARN: ignoring function mismatch entry0')
-        #             else:
-        #                 print('ERR. Target function {} does not match {}'.format(target_func, func_info['name']))
-        #                 error = True
-        #         offset = func_info['offset']
-        #         size = func_info['size']
-        #         patch = {'hashaddr_placeholder': hashaddr_placeholder,
-        #                 'size_placeholder': size_placeholder,
-        #                 'hash_placeholder': hash_placeholder,
-        #                 'checkee_addr': offset,
-        #                 'size_target': size,
-        #                 'expected_hash': 0, 'dummy':error}
-        #         patches.append(patch)
         else:
-            pprint(funcs)
+            # pprint(funcs)
             print('ERR: failed to find function:{}'.format(target_func))
             return False
     if len(patches) != len(guide_content):
@@ -307,9 +279,6 @@ def main(argv):
         guide_content = f.readlines()
     guide_content = [x.strip() for x in guide_content]
     dump_debug_info( 'content: {}'.format(guide_content))
-
-    # r2 = r2pipe.open(args.input_binary)
-    # r2.cmd("aa")
 
     patches = get_patches(args.input_binary, guide_content)
     if not patches:
